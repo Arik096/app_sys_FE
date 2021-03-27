4f2592d1a8b18dc0276b4a970ebd37acf8f06c4a
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-03-27 18:49:05 +0600

    vue bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 createApp(App).use(router).mount('#app')
