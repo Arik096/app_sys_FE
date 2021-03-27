97ec9fccac57ec14a25cc2b47706774d0e3a2c7a
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-03-27 19:04:45 +0600

    vue bootstrap removed

--- src/main.js
+++ src/main.js
@@ -2,12 +2,4 @@ import Vue, { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
 createApp(App).use(router).mount('#app')
