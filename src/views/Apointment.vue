12d51e3d5fb88fd990373eeb9fa0c7cc19f287a5
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-04-03 20:41:12 +0600

    slot done and existing appointment check done

--- src/views/Apointment.vue
+++ src/views/Apointment.vue
@@ -1,8 +1,8 @@
 <template>
   <h1>Make your Appointment</h1>
   <div class="container">
-    <div class="col" id="main-col">
-      <div class="col-sm-5" id="main-row">
+    <div class="row" id="main-row">
+      <div class="col-sm-6" id="main-col">
         <form>
           <div class="form-floating mb-3">
             <input type="text" class="form-control" id="floatingInput" />
@@ -17,10 +17,12 @@
             <label for="floatingInput">Sickness Details</label>
           </div>
           <div class="input-group mb-3">
-            <label class="input-group-text"
-              >Departmemt</label
+            <label class="input-group-text">Departmemt</label>
+            <select
+              class="form-select"
+              v-model="SelectedDepartmentName"
+              @change="ComputeTheDoctorList()"
             >
-            <select class="form-select" v-model="SelectedDepartmentName" @change="ComputeTheDoctorList()">
               <option
                 v-for="department in departments"
                 :key="department.id"
@@ -32,7 +34,11 @@
           </div>
           <div class="input-group mb-3">
             <label class="input-group-text">Doctor</label>
-            <select class="form-select" v-model="SelectedDoctorName" @change="ComputeTheDoctorSlot()">
+            <select
+              class="form-select"
+              v-model="SelectedDoctorName"
+              @change="ComputeTheDoctorSlot()"
+            >
               <option
                 v-for="doctor in TheFilteredDoctorList"
                 :key="doctor.id"
@@ -44,21 +50,85 @@
           </div>
           <div class="input-group mb-3">
             <label class="input-group-text">Slot</label>
-            <select class="form-select">
-              <option
-                v-for="doctor in SelectedDoctorSlot"
-                :key="doctor.id"
-                :value="doctor.slot"
-              >
-                {{ doctor.slot }}
-              </option>
-            </select>
+            <div v-for="doctor in SelectedDoctorSlot" :key="doctor.id">
+              <select class="form-select">
+                <option
+                  v-for="slot in doctor.slots"
+                  :key="slot.id"
+                  :value="slot.name"
+                >
+                  {{ slot.name }} {{ slot.startTime }} - {{ slot.endTime }}
+                </option>
+              </select>
+            </div>
           </div>
           <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Choose Date</span>
+            <span class="input-group-text">Choose Date</span>
             <input type="date" class="form-control" />
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Submit
+          </button>
+        </form>
+
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Appointment Status
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">...</div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3" id="main-col">
+        <h3>Check your Appointment Status</h3>
+        <form>
+          <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="floatingInput" />
+            <label for="floatingInput">Phone</label>
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text">Choose Date</span>
+            <input type="date" class="form-control" />
+          </div>
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
@@ -76,7 +146,22 @@ export default {
           department: "Cancer Care Center",
           info: "Consultant (Radiation and clinical Oncology)",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
         {
           id: 2,
@@ -84,7 +169,22 @@ export default {
           department: "Cancer Care Center",
           info: "Consultant ",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
         {
           id: 3,
@@ -92,7 +192,22 @@ export default {
           department: "AIDS",
           info: "Consultant (Radiation and clinical Oncology)",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
         {
           id: 4,
@@ -100,7 +215,22 @@ export default {
           department: "AIDS",
           info: "Consultant",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
         {
           id: 5,
@@ -108,7 +238,22 @@ export default {
           department: "ENT",
           info: "Consultant (Radiation and clinical Oncology)",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
         {
           id: 6,
@@ -116,7 +261,22 @@ export default {
           department: "ENT",
           info: "Consultant",
           image: "Biswajit_Bhattacharjee.jpg",
-          slot: ["Morning", "Evening"],
+          slots: [
+            {
+              id: 1,
+              name: "Morning",
+              startTime: "9:00 AM",
+              endTime: "12:00PM",
+              limit: 20,
+            },
+            {
+              id: 2,
+              name: "Evening",
+              startTime: "2:00 PM",
+              endTime: "6:00PM",
+              limit: 30,
+            },
+          ],
         },
       ],
       departments: [
@@ -136,34 +296,41 @@ export default {
       SelectedDepartmentName: null,
       TheFilteredDoctorList: null,
       SelectedDoctorName: null,
-      SelectedDoctorSlot: null
+      SelectedDoctorSlot: null,
+      ShowModal: 0,
     };
   },
   methods: {
-    ComputeTheDoctorList(){
+    ComputeTheDoctorList() {
       this.TheFilteredDoctorList = null;
       this.TheFilteredDoctorList = this.Doctors.filter(
         (obj) => obj.department === this.SelectedDepartmentName
       );
     },
-    ComputeTheDoctorSlot(){
+    ComputeTheDoctorSlot() {
       this.SelectedDoctorSlot = null;
       this.SelectedDoctorSlot = this.Doctors.filter(
         (obj) => obj.name === this.SelectedDoctorName
       );
-    }
-  }
+    },
+    OpenModal() {
+      this.ShowModal = 1;
+    },
+  },
 };
 </script>
 
 <style>
 #main-col {
-  display: grid;
-  place-items: center;
-}
-#main-row {
   padding: 20px;
+  margin: 20px;
   border: 1px solid green;
   border-radius: 5px;
+  display: grid;
+  place-items: start auto;
+}
+#main-row {
+  display: flex;
+  justify-content: center;
 }
 </style>
